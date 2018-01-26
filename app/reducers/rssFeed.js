2204fca0861249d13dca64a110b4e028b6325588
Ryan Pfister <puhfister@gmail.com>
2018-01-26 11:03:36 -0700

    making sure offline works

--- app/reducers/rssFeed.js
+++ app/reducers/rssFeed.js
@@ -1,6 +1,11 @@
+import { REHYDRATE } from 'redux-persist/constants';
 import { feedTypes } from '../actions';
 
 export default (state = null, action) => {
+  if (action.type === REHYDRATE) {
+    return action.payload.rssFeed || null;
+  }
+
   if (action.type === feedTypes.INIT_FEED) {
     return { ...action.payload };
   }
--- app/store.js
+++ app/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import { persistStore, autoRehydrate } from 'redux-persist';
 import { AsyncStorage } from 'react-native';
 import reducers from './reducers';
+import { getFeed } from './actions';
 
 const store = createStore(
   reducers,
@@ -13,8 +14,8 @@ const store = createStore(
   ),
 );
 
-persistStore(store, { storage: AsyncStorage, whitelist: [] }, () => {
-  // anything to run after store has been loaded from asyncstorage
+persistStore(store, { storage: AsyncStorage, whitelist: ['rssFeed'] }, () => {
+  store.dispatch(getFeed());
 });
 
 export default store;
