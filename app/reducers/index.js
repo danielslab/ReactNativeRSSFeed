c2c84d2d0616e034bf697ecbe0cd543f5ad513f8
Ryan Pfister <puhfister@gmail.com>
2018-01-26 11:04:39 -0700

    renaming rss feed

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import navReducer from './navReducer';
-import rssFeedReducer from './rssFeed';
+import rssFeedReducer from './rssFeedReducer';
 
 
 const rootReducer = combineReducers({
